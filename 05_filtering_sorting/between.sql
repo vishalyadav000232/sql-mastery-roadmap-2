63784c33e4524d583211784ef09cebb66ee5d83a
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-01 08:33:11 +0530

    Enhance between.sql documentation; add detailed explanations and examples for using the BETWEEN operator in SQL

--- 05_filtering_sorting/between.sql
+++ 05_filtering_sorting/between.sql
@@ -1,1 +1,14 @@
-# between.sql
+
+
+-- The BETWEEN operator is used to filter data within a specific range. The syntax for using the BETWEEN operator is as follows:
+
+-- SELECT column1, column2 FROM table_name
+-- WHERE column_name BETWEEN value1 AND value2;
+
+-- Example :
+
+SELECT * FROM employees
+WHERE salary BETWEEN 50000 AND 100000;
+
+-- In this example, we are selecting all columns from the employees table where the salary is between 50000 and 100000. This allows us to retrieve rows that have values within the specified range.
+  
