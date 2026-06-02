1e7312085cf9d30ba1dbc25a7b94f17a09d31f16
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-02 17:12:05 +0530

    add detailed explanations and examples for using GROUP BY with aggregate functions

--- 06_aggregate_functions/group_by.sql
+++ 06_aggregate_functions/group_by.sql
@@ -1,1 +1,42 @@
-# group_by.sql
+
+-- GROUP AGREEGATE FUNCTIONS
+
+
+-- Group by is used to combine a similer value into the single value. 
+--After grouping, we usually apply Aggregate Functions like:
+-- MAX
+-- SUM
+-- MIN
+-- AVG
+-- COUNT
+
+
+
+
+
+SELECT department, AVG(salary)
+FROM employee
+GROUP BY department;
+
+
+
+--- GROUP BY  + ORDER BY --> Sortin the data on the basis of group 
+
+
+
+
+SELECT department,
+       AVG(salary) AS avg_salary
+FROM employee
+GROUP BY department
+ORDER BY avg_salary DESC;
+
+--- GROUP BY + HAVING  --> filter the group using the having with aggregate function s
+
+
+
+SELECT department,
+       AVG(salary)
+FROM employee
+GROUP BY department
+HAVING AVG(salary) > 40000;
