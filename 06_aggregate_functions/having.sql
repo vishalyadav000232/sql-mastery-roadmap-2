b8ba8e3b012f33d4c51b609fcd303b20405b9ce7
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-02 17:14:53 +0530

    Add detailed explanations and examples for using HAVING with aggregate functions

--- 06_aggregate_functions/having.sql
+++ 06_aggregate_functions/having.sql
@@ -1,1 +1,23 @@
-# having.sql
+
+
+
+-- HAVING AGREEGATE FUCITONS
+
+
+-- HAVING is used to filter groups after GROUP BY.
+
+
+SELECT department,
+       AVG(salary) AS avg_salary
+FROM employee
+GROUP BY department
+HAVING AVG(salary) > 50000;
+
+-- WHERE AND HVING TOGEATHER  
+
+SELECT department,
+       AVG(salary)
+FROM employee
+WHERE salary > 30000
+GROUP BY department
+HAVING AVG(salary) > 50000;
