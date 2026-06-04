6c4bda3f0c2fc4a2c31980d886b4dd107a5b622a
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-04 18:54:31 +0530

    Add detailed explanation and examples for SELF JOIN usage in SQL

--- 07_joins/self_join.sql
+++ 07_joins/self_join.sql
@@ -1,1 +1,38 @@
-# self_join.sql
+
+
+-- 🤔 Why Do We Need SELF JOIN?
+
+-- Suppose an employee's manager is also an employee.🤔 Why Do We Need SELF JOIN?
+
+-- Suppose an employee's manager is also an employee.
+
+
+-- Problem
+
+-- | employee_id | employee_name | manager_id |
+-- -- | ----------- | ------------- | ---------- |
+-- -- | 1           | CEO           | NULL       |
+-- -- | 2           | Vishal        | 1          |
+-- -- | 3           | Rahul         | 1          |
+-- -- | 4           | Amit          | 2          |
+-- -- | 5           | Neha          | 2          |
+
+
+
+-- | Employee | Manager |
+-- | -------- | ------- |
+-- | Vishal   | CEO     |
+-- | Rahul    | CEO     |
+-- | Amit     | Vishal  |
+-- | Neha     | Vishal  |
+
+
+-- We need to connect the table to itself.
+
+
+SELECT
+    e.employee_name AS employee,
+    m.employee_name AS manager
+FROM employees e
+LEFT JOIN employees m
+ON e.manager_id = m.employee_id;
