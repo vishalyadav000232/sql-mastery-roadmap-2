3f741f91fdb04f1a43a0fa06e68cc964c487a888
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-04 18:43:40 +0530

    Add explanations and examples for LEFT JOIN usage in SQL

--- 07_joins/left_join.sql
+++ 07_joins/left_join.sql
@@ -1,1 +1,42 @@
-# left_join.sql
+
+
+
+
+-- What is LEFT JOIN?
+
+-- LEFT JOIN returns:
+-- ✅ All records from the left table
+-- ✅ Matching records from the right table
+-- ✅ If no match exists in the right table, SQL returns NULL
+
+
+
+-- SELECT columns
+-- FROM table1
+-- LEFT JOIN table2
+-- ON table1.column = table2.column;
+
+
+
+
+SELECT
+    e.employee_id,
+    e.name,
+    d.department_name
+FROM employees e
+LEFT JOIN departments d
+ON e.department_id = d.department_id;
+
+
+
+
+-- Count Employees in Each Department
+
+
+SELECT
+    d.department_name,
+    COUNT(e.employee_id) AS total_employees
+FROM departments d
+LEFT JOIN employees e
+ON d.department_id = e.department_id
+GROUP BY d.department_name;
