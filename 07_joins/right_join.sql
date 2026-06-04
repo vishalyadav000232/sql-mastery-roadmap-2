bbe74363152e084020857870ae891522000c29b0
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-04 18:46:23 +0530

    Add explanations and examples for RIGHT JOIN usage in SQL

--- 07_joins/right_join.sql
+++ 07_joins/right_join.sql
@@ -1,1 +1,38 @@
-# right_join.sql
+-- What is RIGHT JOIN?
+
+-- RIGHT JOIN returns
+-- ✅ All records from the right table
+-- ✅ Matching records from the left table
+-- ✅ If no match exists in the left table, SQL returns NULL
+-- Think of it as the opposite of LEFT JOIN.
+
+
+
+/*
+
+SELECT columns
+FROM employees e
+RIGHT JOIN departments d
+ON e.department_id = d.department_id;
+
+*/
+
+
+SELECT columns
+FROM employees e
+RIGHT JOIN departments d
+ON e.department_id = d.department_id;
+
+
+
+-- RIGHT JOIN + Aggregate Functions
+
+
+
+SELECT
+    d.department_name,
+    COUNT(e.employee_id) AS total_employees
+FROM employees e
+RIGHT JOIN departments d
+ON e.department_id = d.department_id
+GROUP BY d.department_name;
