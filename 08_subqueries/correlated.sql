54cc80df8d6409e840fb2dbe67d3b81ec385cc18
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-05 12:09:43 +0530

    Add definition and explanation for correlated subqueries in SQL

--- 08_subqueries/correlated.sql
+++ 08_subqueries/correlated.sql
@@ -1,1 +1,30 @@
-# correlated.sql
+
+
+-- 🎯 Definition
+
+-- A Correlated Subquery is a subquery that depends on the outer query.
+
+-- It cannot run independently.
+
+-- The inner query executes once for every row processed by the outer query.
+
+
+-- Normal SubQuery 
+
+SELECT *
+FROM employees
+WHERE salary >
+(
+    SELECT AVG(salary)
+    FROM employees
+);
+
+-- correlated subquerry  
+SELECT *
+FROM employees e1
+WHERE salary >
+(
+    SELECT AVG(salary)
+    FROM employees e2
+    WHERE e2.dept_id = e1.dept_id
+);
