dedc876b9192859845246e4a6772af991d2b6594
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-05 12:45:39 +0530

    Add recursive Common Table Expressions (CTEs) examples for number generation and employee hierarchy

--- 09_cte_common_table_expressions/recursive_cte.sql
+++ 09_cte_common_table_expressions/recursive_cte.sql
@@ -1,1 +1,62 @@
-# recursive_cte.sql
+
+
+
+
+
+
+
+
+
+
+
+WITH RECURSIVE numbers AS (
+
+    SELECT 1
+
+    UNION ALL
+
+    SELECT n + 1
+    FROM numbers
+    WHERE n < 10
+)
+
+SELECT *
+FROM numbers;
+
+
+---- output 
+
+-- 1
+-- 2
+-- 3
+-- 4
+-- 5
+-- 6
+-- 7
+-- 8
+-- 9
+-- 10
+
+
+WITH RECURSIVE employee_tree AS (
+
+    SELECT
+        id,
+        name,
+        manager_id
+    FROM employees
+    WHERE manager_id IS NULL
+
+    UNION ALL
+
+    SELECT
+        e.id,
+        e.name,
+        e.manager_id
+    FROM employees e
+    JOIN employee_tree et
+    ON e.manager_id = et.id
+)
+
+SELECT *
+FROM employee_tree;
