189e4abe7c716772021a52264b4718f7e6eb51cf
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-05 08:45:35 +0530

    Add detailed explanation and examples for multiple row subqueries in SQL

--- 08_subqueries/multi_row.sql
+++ 08_subqueries/multi_row.sql
@@ -1,1 +1,46 @@
-# multi_row.sql
+
+
+-- Multiple row subquery 
+
+
+-- A Multi-Row Subquery is a subquery that returns more than one row (multiple values).
+
+-- Unlike a Single Row Subquery, the outer query must compare against multiple values, so operators like IN, ANY, ALL, and EXISTS are used.
+
+SELECT *
+FROM students
+WHERE course_id IN
+(
+    SELECT course_id
+    FROM courses
+    WHERE status='ACTIVE'
+);
+
+SELECT *
+FROM employees
+WHERE salary >
+ANY
+(
+    SELECT salary
+    FROM employees
+    WHERE dept_id=2
+);
+
+SELECT *
+FROM employees
+WHERE salary >
+ALL
+(
+    SELECT salary
+    FROM employees
+    WHERE dept_id=2
+);
+
+SELECT *
+FROM departments d
+WHERE EXISTS
+(
+    SELECT *
+    FROM employees e
+    WHERE e.dept_id=d.dept_id
+);
