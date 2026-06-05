f2348e508c0eff1881bd1531ef3edfc28bc27275
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-05 08:52:04 +0530

    Add detailed comments and examples for nested subqueries in SQL

--- 08_subqueries/nested.sql
+++ 08_subqueries/nested.sql
@@ -1,1 +1,55 @@
-# nested.sql
+
+
+-- A Nested Subquery is a subquery that contains another subquery inside it.
+
+-- Outer Query
+--    └── Subquery
+--           └── Subquery
+
+
+
+
+/*
+
+
+SELECT ...
+FROM ...
+WHERE column =
+(
+    SELECT ...
+    FROM ...
+    WHERE column =
+    (
+        SELECT ...
+        FROM ...
+    )
+);
+
+*/
+
+-- Mujhe un employees ki list do jo Rahul ke department mein kaam karte hain."
+
+
+SELECT *
+FROM employees
+WHERE dept_id =
+(
+    SELECT dept_id
+    FROM employees
+    WHERE name='Rahul'
+);
+
+
+SELECT *
+FROM employees
+WHERE dept_id =
+(
+    SELECT dept_id
+    FROM departments
+    WHERE manager_id =
+    (
+        SELECT emp_id
+        FROM employees
+        WHERE name='Rahul'
+    )
+);
