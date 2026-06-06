31f9c97730314b83a6f0baa957147fd8216ebb1e
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-06 17:16:27 +0530

    Add detailed comments and explanations for ROW_NUMBER() window function

--- 10_window_functions/row_number.sql
+++ 10_window_functions/row_number.sql
@@ -1,1 +1,20 @@
-# row_number.sql
+
+
+
+
+--- ROW_NUMBER(). Window function 
+
+-- give   a unique  number in every row 
+-- Even two value are same , numbers will be different 
+
+SELECT 
+  name,
+  marks,
+  ROW_NUMBER() OVER (ORDER BY marks DESC) AS row_no
+FROM students;
+
+-- | name  | marks | row_no |
+-- | ----- | ----: | -----: |
+-- | Rahul |    95 |      1 |
+-- | Aman  |    90 |      2 |
+-- | Ravi  |    90 |      3 |
