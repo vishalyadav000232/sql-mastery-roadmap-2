acd00763f29b3465fc92582aee04a75d3dde94d8
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-06 17:22:37 +0530

    Add detailed comments and formatting to DENSE_RANK() window function example

--- 10_window_functions/dense_rank.sql
+++ 10_window_functions/dense_rank.sql
@@ -1,1 +1,33 @@
-# dense_rank.sql
+
+
+
+-- DENSE_RANK(). WINDOW FUNCTION 
+
+
+
+-- give the ranking but it does not skip the ranke 
+
+SELECT 
+  name,
+  marks,
+  DENSE_RANK() OVER (ORDER BY marks DESC) AS dense_rank
+FROM students;
+
+-- | name  | marks | dense_rank |
+-- | ----- | ----: | ---------: |
+-- | Rahul |    95 |          1 |
+-- | Aman  |    90 |          2 |
+-- | Ravi  |    90 |          2 |
+-- | Mohan |    85 |          3 |
+
+
+
+
+-- Difference b/w rank vs dense_rank
+
+-- | marks | RANK | DENSE_RANK |
+-- | ----: | ---: | ---------: |
+-- |    95 |    1 |          1 |
+-- |    90 |    2 |          2 |
+-- |    90 |    2 |          2 |
+-- |    85 |    4 |          3 |
