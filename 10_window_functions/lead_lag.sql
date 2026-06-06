5a7259f31d6ff87b3662c3b2297557178218e4e3
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-06 17:26:56 +0530

    Add comments and formatting to LAG() and LEAD() window function examples

--- 10_window_functions/lead_lag.sql
+++ 10_window_functions/lead_lag.sql
@@ -1,1 +1,38 @@
-# lead_lag.sql
+-- LAG() gives previous row value.
+
+
+
+SELECT 
+  student_id,
+  payment_date,
+  amount,
+  LAG(amount) OVER (
+    PARTITION BY student_id
+    ORDER BY payment_date
+  ) AS previous_payment
+FROM payments;
+
+
+-- | student_id | amount | previous_payment |
+-- | ---------: | -----: | ---------------: |
+-- |          1 |    500 |             NULL |
+-- |          1 |    700 |              500 |
+-- |          1 |   1000 |              700 |
+-- -- 
+
+
+
+-- LEAD()
+
+-- LEAD() gives next row value.
+
+
+SELECT 
+  student_id,
+  payment_date,
+  amount,
+  LEAD(amount) OVER (
+    PARTITION BY student_id
+    ORDER BY payment_date
+  ) AS next_payment
+FROM payments;
