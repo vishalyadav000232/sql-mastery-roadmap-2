1240b21cf543ced985a0577f058671246f66e54f
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-06 17:24:53 +0530

    Add comments and formatting to PARTITION BY window function example

--- 10_window_functions/partition_by.sql
+++ 10_window_functions/partition_by.sql
@@ -1,1 +1,30 @@
-# partition_by.sql
+
+
+
+-- PARTITION BY  window function 
+
+
+-- PARTITION BY divides data into groups.
+
+-- It works like GROUP BY, but does not reduce rows.
+
+SELECT 
+  student_name,
+  shift,
+  marks,
+  RANK() OVER (
+    PARTITION BY shift
+    ORDER BY marks DESC
+
+
+
+  ) AS shift_rank
+FROM students;
+
+
+-- | student | shift   | marks | shift_rank |
+-- | ------- | ------- | ----: | ---------: |
+-- | Rahul   | Morning |    95 |          1 |
+-- | Aman    | Morning |    90 |          2 |
+-- | Ravi    | Evening |    96 |          1 |
+-- | Mohan   | Evening |    85 |          2 |
