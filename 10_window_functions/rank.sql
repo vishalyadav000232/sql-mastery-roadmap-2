d965ebebec265ed16bbf49b649fbb869dc8d9090
VishaL Yadav <vishalyadav000232@gmail.com>
2026-06-06 17:18:56 +0530

    Add comments and formatting to RANK() window function example

--- 10_window_functions/rank.sql
+++ 10_window_functions/rank.sql
@@ -1,1 +1,26 @@
-# rank.sql
+
+
+
+
+
+
+
+-- RANK(). Window Function 
+
+
+
+--= give the ranking , but if two value same it skip the next rank 
+
+
+SELECT 
+  name,
+  marks,
+  RANK() OVER (ORDER BY marks DESC) AS rank
+FROM students;
+
+-- | name  | marks | rank |
+-- | ----- | ----: | ---: |
+-- | Rahul |    95 |    1 |
+-- | Aman  |    90 |    2 |
+-- | Ravi  |    90 |    2 |
+-- | Mohan |    85 |    4 |
